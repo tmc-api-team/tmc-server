daa907b9de0b93ee148a0cdacf346edb8fb24550
Antti Leinonen <antti.leinonen@helsinki.fi>
2016-10-11 15:46:56 +0300

    Update authenticate_user! method

--- app/controllers/api/v7/base_controller.rb
+++ app/controllers/api/v7/base_controller.rb
@@ -1,11 +1,10 @@
 require 'json'
 
 class Api::V7::BaseController < ApplicationController
-
   clear_respond_to
   respond_to :json
 
-#  before_action :doorkeeper_authorize!
+  #  before_action :doorkeeper_authorize!
   before_action :authenticate_user!
 
   skip_authorization_check # we use doorkeeper in api, so let's skip cancancan
@@ -29,18 +28,15 @@ class Api::V7::BaseController < ApplicationController
   private
 
   def authenticate_user!
+    return @current_user if @current_user
     if doorkeeper_token
-      @current_user = User.find(doorkeeper_token.resource_owner_id)
+      @current_user ||= User.find(doorkeeper_token.resource_owner_id)
+      raise 'Invalid token' unless @current_user
     end
-
-    return if @current_user
-
-    render json: { errors: ['User is not authenticated!'] }, status: :unauthorized
+    @current_user ||= Guest.new
   end
 
-  def current_user
-    @current_user
-  end
+  attr_reader :current_user
 
   def errors_json(messages)
     { errors: [*messages] }
