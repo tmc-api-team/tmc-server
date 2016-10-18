7d4953159af0bc019697bb43d938a71c69543600
Antti Leinonen <antti.leinonen@helsinki.fi>
2016-10-18 16:18:15 +0300

    Testing for Api v7 ExercisesController

--- app/controllers/api/v7/exercises_controller.rb
+++ app/controllers/api/v7/exercises_controller.rb
@@ -29,45 +29,7 @@ class Api::V7::ExercisesController < Api::V7::BaseController
         send_file @exercise.stub_zip_file_path
       end
       format.json do
-        # This is used by (at least) tmc.py at the moment
-        return render json: { error: 'Authentication required to view courses.' }, status: 403 if current_user.guest?
-
-        @submissions = @exercise.submissions.order('submissions.created_at DESC')
-        @submissions = @submissions.where(user_id: current_user.id) unless current_user.administrator?
-        @submissions = @submissions.includes(:awarded_points).includes(:user)
-        authorize! :read, @submissions
-
-        data = {
-          course_name:                      @course.name,
-          course_id:                        @course.id,
-          code_review_requests_enabled:     @exercise.code_review_requests_enabled?,
-          run_tests_locally_action_enabled: @exercise.run_tests_locally_action_enabled?,
-          exercise_name:                    @exercise.name,
-          exercise_id:                      @exercise.id,
-          unlocked_at:                      @exercise.time_unlocked_for(current_user),
-          deadline:                         @exercise.deadline_for(current_user),
-          submissions:                      SubmissionList.new(current_user, view_context).submission_list_data(@submissions),
-        }
-        render json: data.to_json
       end
     end
-  end
-
-  def set_disabled_statuses
-    @course = Course.find(params[:course_id])
-    @organization = @course.organization
-    authorize! :teach, @organization
-
-    selected_exercise_ids = params[:course][:exercises]
-    selected_exercise_ids.reject!(&:blank?)
-
-    course_exercises = Exercise.where(course: @course)
-    selected_exercises = Exercise.where(id: selected_exercise_ids, course: @course)
-
-    course_exercises.update_all(disabled_status: true)
-    selected_exercises.update_all(disabled_status: false)
-
-    redirect_to manage_exercises_organization_course_path(@organization, @course),
-                notice: 'Exercises successfully updated.'
   end
 end
---
+++ spec/controllers/api/v7/exercises_controller_spec.rb
@@ -0,0 +1,32 @@
+require 'spec_helper'
+
+describe Api::V7::ExercisesController, type: :controller do
+  describe 'GET show' do
+    before :each do
+      repo_path = Dir.pwd + '/remote_repo'
+      create_bare_repo(repo_path)
+      @organization = FactoryGirl.create(:accepted_organization, slug: 'slug')
+      @course = Course.create!(name: 'mycourse', title: 'mycourse', source_backend: 'git', source_url: repo_path, organization: @organization)
+      @repo = clone_course_repo(@course)
+      @repo.copy_simple_exercise('MyExercise')
+      @repo.add_commit_push
+
+      @course.refresh
+    end
+    let!(:exercise) { FactoryGirl.create(:exercise, course: @course) }
+
+    describe 'with .zip format' do
+      it 'should offer the exercise as a downloadable zip', driver: :rack_test do
+        visit '/api/v7/exercises/1.zip'
+
+        File.open('MyExercise.zip', 'wb') { |f| f.write(page.source) }
+        system!('unzip -qq MyExercise.zip')
+
+        expect(File).to be_a_directory('MyExercise')
+        expect(File).to exist('MyExercise/src/SimpleStuff.java')
+        expect(File).to exist('MyExercise/test/SimpleTest.java')
+        expect(File).not_to exist('MyExercise/test/SimpleHiddenTest.java')
+      end
+    end
+  end
+end
