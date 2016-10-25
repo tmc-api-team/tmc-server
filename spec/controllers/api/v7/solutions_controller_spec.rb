7f5b5fdece2227be22c6e0c56daf4fd35f6fd3dc
Nea Pirttinen <nea.pirttinen@cs.helsinki.fi>
2016-10-25 10:28:48 +0300

    Test for API v7 solutions controller

---
+++ spec/controllers/api/v7/solutions_controller_spec.rb
@@ -0,0 +1,40 @@
+require 'spec_helper'
+require 'fileutils'
+
+describe Api::V7::SolutionsController, type: :controller do
+
+  describe 'GET show' do
+    before :each do
+      repo_path = Dir.pwd + '/remote_repo'
+      create_bare_repo(repo_path)
+      @organization = FactoryGirl.create(:accepted_organization, slug: 'slug')
+      @course = Course.create!(name: 'mycourse', title: 'mycourse', source_backend: 'git', source_url: repo_path, organization: @organization)
+      @repo = clone_course_repo(@course)
+      @repo.copy_simple_exercise('MyExercise')
+      @repo.add_commit_push
+
+      @course.refresh
+    end
+
+    describe 'with .zip format' do
+
+      let(:token) { Doorkeeper::AccessToken.create!(:resource_owner_id => @user.id).token }
+
+      it 'should offer the solution as a downloadable zip', driver: :rack_test do
+        visit "/api/v7/exercises/#{@course.exercises.find_by(name: "MyExercise").id}.zip?access_token=#{:token}"
+
+        File.open('MyExercise.zip', 'wb') { |f| f.write(page.source) }
+        system!('unzip -qq MyExercise.zip')
+
+        expect(File).to be_a_directory('MyExercise')
+        expect(File).to exist('MyExercise/src/SimpleStuff.java')
+        expect(File).to exist('MyExercise/test/SimpleTest.java')
+        expect(File).not_to exist('MyExercise/test/SimpleHiddenTest.java')
+      end
+    end
+
+    after :all do
+      FileUtils.rm_rf('test_tmp_dir')
+    end
+  end
+end
